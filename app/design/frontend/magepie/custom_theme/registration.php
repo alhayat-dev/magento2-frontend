915168002587d577af37a704881bbedac4a6bc07
Mudasser Hayat <alhayat.dev@gmail.com>
2021-03-13 06:55:52 +0500

    Created custom theme.

---
+++ app/design/frontend/magepie/custom_theme/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/magepie/custom_theme',
+    __DIR__
+);
